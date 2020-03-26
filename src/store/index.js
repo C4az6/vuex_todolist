9fe8a97d00d2ef2ab08973b4844fc703a4e6d41a
replaceroot <1401792880@qq.com>
2020-03-26 14:18:44 +0800

    实现内容的动态渲染

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 所有的任务列表
+    list: []
   },
   mutations: {
+    initList (state, list) {
+      state.list = list
+    }
   },
   actions: {
+    getList (context) {
+      axios.get('/list.json')
+        .then(({ data }) => {
+          context.commit('initList', data)
+        })
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
